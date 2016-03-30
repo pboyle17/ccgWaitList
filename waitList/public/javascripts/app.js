d8e1e093af68b4c786faf9c2b2afc964c9e3ab94
Paul Boyle <pboyle17@gmail.com>
2016-03-30 17:50:38 -0500

    scrolling page working with alerts

--- waitList/public/javascripts/app.js
+++ waitList/public/javascripts/app.js
@@ -1,6 +1,48 @@
 $(document).ready(function(){
 
+  var scroll = function(){
+    console.log('scroll function initiated');
+    console.log(window.location.hash);
+    if(window.location.hash =='#13NL'){
+      console.log('changing location');
+      window.location.hash='#125PLO';
+      console.log(window.location.hash);
+    } else if (window.location.hash == '#125PLO') {
+      window.location.hash='#11NL';
+    } else if(window.location.hash == '#11NL'){
+      window.location.hash = '#13NL';
+    }
+  };
+
+  var scrollingPage=setInterval(function () {
+      scroll();
+    }, 5000);
+
+  var stop = function(){
+    clearInterval(scrollingPage);
+  }
+
+  $('.resume').click(function(){
+    console.log('resuming scroll');
+    window.location.hash = '#13NL';
+    $('#message').addClass('alert-info');
+    $('#message').html('scroll has been resumed');
+    scrollingPage();
+  });
+
+  $('.stop').click(function(){
+    console.log('stopping scroll on the page');
+    $('#message').addClass('alert-info');
+    $('#message').html('scroll has been stopped');
+    setTimeout(function () {
+      $('#message').removeClass('alert-info');
+      $('#message').html('');
+    }, 3000);
+    stop();
+  });
+
   $('.delete').click(function(){
+    $('#message').addClass('alert-danger');
     $('#message').html('removing player from wait list .....');
     $.ajax({
       url:'/'+this.id,
@@ -32,6 +74,7 @@ $(document).ready(function(){
 
   $('#add').click(function(){
     var form = document.getElementById('name');
+    $('#message').addClass('alert-success');
     $('#message').html('adding player...'+form.value);
   });
 
